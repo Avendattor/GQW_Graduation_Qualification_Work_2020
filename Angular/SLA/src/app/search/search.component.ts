bdcf1d9a81307a5c14ce5b80ceadcbb5092510ff
Paul <47778996+Avendattor@users.noreply.github.com>
2020-04-28 19:58:22 +0300

    CSS fixes, search visual improvements

--- Angular/SLA/src/app/search/search.component.ts
+++ Angular/SLA/src/app/search/search.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  input_searchByIP: string;
+  input_searchByMAC: string;
+
   constructor() { }
 
   ngOnInit(): void {
