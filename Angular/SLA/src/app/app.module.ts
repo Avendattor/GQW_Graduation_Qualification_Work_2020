6b7b4487eefb5a0d09951552c3cf6cce83bd6c8a
Paul <47778996+Avendattor@users.noreply.github.com>
2020-05-24 18:39:00 +0300

    + Angular Material Buttons import

--- Angular/SLA/src/app/app.module.ts
+++ Angular/SLA/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MainTopComponent } from './main-top/main-top.component';
 import { SearchComponent } from './search/search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -34,6 +35,7 @@ import { MatTableModule } from '@angular/material/table';
     MatFormFieldModule,
     BrowserAnimationsModule,
     MatTableModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
