1af6b303d4d23c651e247fba89f1d1754d867a8e
Paul <47778996+Avendattor@users.noreply.github.com>
2020-06-07 04:59:41 +0300

    SLA: + logout button, works by refreshing page

--- Angular/SLA/src/app/top-bar/top-bar.component.ts
+++ Angular/SLA/src/app/top-bar/top-bar.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
+
+
+
 export class TopBarComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @Input() hideLogin: boolean;
+
+  refresh(): void {
+    window.location.reload();
   }
 
 }
